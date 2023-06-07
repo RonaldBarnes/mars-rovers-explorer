a306e44e8493a762599b051ab21c38f9da34bbcb
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-07 13:24:07 -0700

    Remove context from index.js Remove tanstack & query from index.js Major refactor...
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import ResourceProvider from "./common/ResourceProvider";
-import CameraContext from "./features/cameras/CameraContext";
-import PhotoContext from "./features/photos/PhotoContext";
-import RoverContext from "./features/rovers/RoverContext";
-import App from "./app/App";
 
-const queryClient = new QueryClient();
+import App from "./app/App";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
+
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ResourceProvider resource="cameras" resourceContext={CameraContext}>
-        <ResourceProvider resource="photos" resourceContext={PhotoContext}>
-          <ResourceProvider resource="rovers" resourceContext={RoverContext}>
-            <App />
-          </ResourceProvider>
-        </ResourceProvider>
-      </ResourceProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+	);
