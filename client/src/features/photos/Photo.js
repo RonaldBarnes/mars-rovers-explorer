54e71ded04b055063907fbe14b1e336b76a7b17d
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-09 01:19:04 -0700

    Mostly CSS styling.
    Further work to fetch photos required.
    
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- client/src/features/photos/Photo.js
+++ client/src/features/photos/Photo.js
@@ -1,4 +1,5 @@
 export default function Photo({ source }) {
+console.log(`Photo.js ${source}`)
   return (
     <>
       <img alt="Mars" src={source} />
--- client/src/features/photos/PhotoList.js
+++ client/src/features/photos/PhotoList.js
@@ -1,16 +1,27 @@
-import usePhotoContext from "./usePhotoContext";
+// import usePhotoContext from "./usePhotoContext";
 import Photo from "./Photo";
+import "./PhotoList.css";
 
-export default function PhotoList({ display }) {
-  const photos = usePhotoContext();
 
-  const displayAllPhotos = () => photos.map((photo) => (
-    <Photo source={photo.source} key={photo.id} />
-  ));
+// http://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos?sol=1000&page=2
+// http://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos?earth_date=2015-6-3
+// http://mars-photos.herokuapp.com/api/v1/rovers/opportunity/photos?earth_date=2015-6-3&camera=pancam
+// http://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos?sol=1000
 
-  return (
-    <div className="PhotoList" style={{ display: display }}>
-      { displayAllPhotos() }
-    </div>
-  );
-};
+
+export default function PhotoList({camera_id, cameraName = "?"})
+	{
+	// const photos = usePhotoContext();
+console.log(`PhotoList.js photos  camera_id: ${camera_id}`)
+
+	// const displayAllPhotos = () => photos.map((photo) => (
+	//   <Photo source={photo.source} key={photo.id} />
+	// ));
+
+	return (
+		<div className="PhotoList">
+			{/* { displayAllPhotos() } */}
+PhotoList... camera_id = "{camera_id}"  cameraName = "{cameraName}"
+		</div>
+		);	// end return
+	};	// end PhotoList
