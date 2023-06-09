6a00e7f7092fabd7c7e030aefba4c1e98b9e9f16
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-09 01:06:05 -0700

    Removed some Contexts imports. Created new rovers Context. Assign profile_pic to each rover to pass along as Context. Made use of custom useFetch hook (which also uses useAsync hook): 	pure JS, no tanstack, etc. required. Set API URL as constant and export it.
    CSS styling.
    
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,27 +1,45 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import useCameraContext from "../features/cameras/useCameraContext";
-import usePhotoContext from "../features/photos/usePhotoContext";
-import useRoverContext from "../features/rovers/useRoverContext";
-import NavigationRoutes from "../common/NavigationRoutes";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import useFetch from "../hooks/useFetch";
+
+import MyRoutes from "../common/Routes";
 
 import "./App.css";
 
-function App() {
-  const cameras = useCameraContext();
-  const photos = usePhotoContext();
-  const rovers = useRoverContext();
-  
-  if (!cameras || !photos || !rovers) {
-    return "loading...";
-  }
+export const RoverContext = React.createContext();
 
-  return (
-    <Router>
-      <div id="App">
-        <NavigationRoutes />
-      </div>
-    </Router>
-  );
-};
+// Variables for API data and for photos of rovers:
+const MARS_DATA_URL = "https://mars-photos.herokuapp.com";
+export const API_URL = `${MARS_DATA_URL}/api/v1`;
 
-export default App;
+
+export default function App() {
+	// Fetch list of rovers' data:
+	const { loading, error, value } = useFetch(`${API_URL}/rovers/`, {}, [] );
+
+	// Extract the array from the object containing array of rovers:
+	let rovers;
+	// value === undefined until async fetch is resolved:
+	if (value !== undefined)
+		{
+		rovers = value.rovers;
+		// The profile photo is not included in a rover object; add it to each:
+		rovers = rovers.map( r => {
+			r.profile_pic=`${MARS_DATA_URL}/explore/images/${r.name}_rover.jpg`
+			return r;
+			})
+		}
+
+	return (
+		<>
+			<RoverContext.Provider value={{ loading, error, rovers }} >
+				<Router>
+						<div id="App">
+							<MyRoutes />
+						</div>
+				</Router>
+			</RoverContext.Provider>
+		</>
+		);	// end return
+	};	// end App
