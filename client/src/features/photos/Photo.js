f823c40882fa8803ccc44c2d8fd0be531def6660
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-13 11:21:07 -0700

    Alt and title text on photos.
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- client/src/features/photos/Photo.js
+++ client/src/features/photos/Photo.js
@@ -1,8 +1,25 @@
-export default function Photo({ source }) {
-console.log(`Photo.js ${source}`)
-  return (
-    <>
-      <img alt="Mars" src={source} />
-    </>
-  );
-};
+// export default function PhotoFUCKYOU({ source, photo }) {
+export default function Photo({ source, photo })
+	{
+	console.log(`Photo.js ${source}`)
+
+	const imageInfo = `Image Info:
+	Earth Date: ${photo.earth_date}
+	Sol: ${photo.sol}
+	Camera: ${photo.camera.full_name}
+	Rover: ${photo.rover.name}
+	photo id: ${photo.id}`;
+
+	return (
+		<div className="Photo">
+			<figure>
+			<img
+				alt={imageInfo}
+				src={photo.img_src}
+				title={imageInfo}
+				/>
+			<figcaption>{photo.earth_date} #{photo.id}</figcaption>
+			</figure>
+		</div>
+		);
+	};
