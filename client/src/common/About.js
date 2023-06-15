8497895dc5b0bd59b2663d0dd1ddb00f613d3780
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-15 13:29:52 -0700

    About.js, About.css: * Assigned grid-area * Added attribution to Peyton's GitHub repo * Added "Obligatory XKCD" * Tweaked styling
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- client/src/common/About.js
+++ client/src/common/About.js
@@ -8,6 +8,29 @@ export default function About() {
     <div id="about">
       <p>This web application derives data from the Mars Photo API maintained by chrisccerami.</p>
       <p>Explore the <a href="https://github.com/chrisccerami/mars-photo-api">official repository</a>.</p>
+      <p>
+        This project was forked from Peyton's at {" "}
+        <a
+          href="https://github.com/pekellogg/react-app"
+          target="_blank"
+          >
+          her Github repo
+        </a>.
+      </p>
+      <hr />
+      <figure style={{textAlign:"center"}}>
+        <figcaption style={{marginBottom:"1rem"}}>
+          <a href="https://xkcd.com/695/"
+            target="_blank"
+            >
+            Obligatory xkcd
+          </a> entitled "<b><i>Spirit</i></b>" (a Very Good Rover):
+        </figcaption>
+        <img src="https://imgs.xkcd.com/comics/spirit.png"
+          alt="XKCD: Mars Rover Spirit: A Good Rover"
+          style={{maxWidth:"75vw"}}
+          />
+      </figure>
     </div>
   );
 };
