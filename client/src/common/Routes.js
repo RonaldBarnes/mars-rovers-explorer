27cae5855e3cf57d1031311b4f922839958923c6
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-22 13:18:15 -0700

    Routes.js: * Removed unused imports (CameraButtonList, PhotoList) * Nested routes with optional items indicated by :...?
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- client/src/common/Routes.js
+++ client/src/common/Routes.js
@@ -4,8 +4,6 @@ import NavBar from "./NavBar";
 import About from "./About";
 import RoverList from "../features/rovers/RoverList";
 import RoverCard from "../features/rovers/RoverCard";
-import CameraButtonList from "../features/cameras/CameraButtonList";
-// import PhotoList from "../features/photos/PhotoList";
 
 
 export default function MyRoutes() {
@@ -14,16 +12,17 @@ export default function MyRoutes() {
 			<NavBar />
 			<Routes>
 				<Route exact path="/" element={<RoverList />} />
-				<Route path="/rovers" element={<RoverList />} />
-				<Route
-					path="/rovers/:rover_name/:camera_id?"
-					element={<RoverCard />}
-					>
-					{/* No need for child / nested route: optional camera_id above. */}
-					{/* <Route path=":camera_id" element={<RoverCard />} /> */}
+				<Route path="/rovers" element={<RoverList />}>
+					<Route
+						path=":rover_name/:camera_id?"
+						element={<RoverCard />}
+						>
+						{/* No need for child / nested route: optional camera_id above. */}
+						{/* <Route path=":camera_id" element={<RoverCard />} /> */}
+					</Route>
 				</Route>
+				<Route path="/photos/:rover_name?" component={<RoverCard />} />
 				<Route path="/about" element={<About />} />
-				{/* <Route path="/photos" component={PhotoList} /> */}
 				{/* Catch any other URL paths, send to About: */}
 				<Route path="/*" element={<About />} />
 			</Routes>
