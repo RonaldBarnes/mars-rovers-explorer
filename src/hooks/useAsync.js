0fd658d89480cac6086d52f699a7f36ce6105dd9
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-25 00:44:17 -0700

    useAsync.js: Tested changes in useCallback dependencies:
    * Issues with dependencies: eslint wants callbackFn included but causes
    infinite re-renders:
    
    [eslint]
    src/hooks/useAsync.js
    	Line 33:6:  React Hook useCallback has a missing dependency: 'callbackFn'.
    	Either include it or remove the dependency array. If 'callbackFn' changes
    	too often, find the parent component that defines it and wrap that
    	definition in useCallback
    	(react-hooks/exhaustive-deps)
    
    * Issue with `dependencies` prop & eslint:
    
    [eslint]
    src/hooks/useAsync.js
    	Line 33:6:  React Hook useCallback was passed a dependency list that is
    	not an array literal. This means we can't statically verify whether you've
    	passed the correct dependencies
    	(react-hooks/exhaustive-deps)
    
    * Modified console.log debugging statements
    
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -5,14 +5,14 @@ import { useState, useEffect, useCallback } from 'react';
 export default function useAsync(callbackFn, dependencies = [])
 	{
 	console.log(`%cuseAsync`, "color:lightblue");
-console.log(`useAsync() dependencies: ${dependencies}`)
+console.log(`useAsync() dependencies: ${dependencies} isArray: ${Array.isArray(dependencies)}`)
 
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState();
 	const [value, setValue] = useState();
 
 	const callbackMemoized = useCallback( () => {
-console.log(`%cuseAsync MEMOIZED`, "color:lightblue");
+		console.log(`%cuseAsync MEMOIZED`, "color:lightblue");
 		setLoading(true);
 		setError(undefined);
 		setValue(undefined);
@@ -20,7 +20,17 @@ console.log(`%cuseAsync MEMOIZED`, "color:lightblue");
 			.then(setValue)
 			.catch(setError)
 			.finally( () => setLoading(false))
-		}, [callbackFn, dependencies]
+			// eslint complains: dependencies is not an array literal.
+			// It is, however, an array and placing inside [] causes infinite re-renders.
+			//
+			// eslint complains: callbackFn is not a dependency, yet adding it causes
+			// looping re-renders:
+			//
+			// Line 27:6:  React Hook useCallback has a missing dependency: 'callbackFn'.
+			// Either include it or remove the dependency array. If 'callbackFn' changes
+			// too often, find the parent component that defines it and wrap that definition
+			// in useCallback  react-hooks/exhaustive-deps
+		}, dependencies//, callbackFn]	// , ]
 		);	// end callbackMemoized
 
 
