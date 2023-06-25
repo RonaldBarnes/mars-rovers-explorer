6df258cc6fa7a221d870410e08e7b4321ad91240
Ronald Barnes <ron@ronaldbarnes.ca>
2023-06-25 03:39:56 -0700

    Photo.js: * Implemented useSearchParams to auto-expand photo_id in URL * Set searchParam to photo_id when clicking on a photo * Changed fullScreen,setFullScreen to use useState to track photo_id, not boolean whether an image is full screen.
    TODO: Styling to place "fullScreen" images better:
    	(in CSS or via JS to determine exact coordinates; undecided)
    
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- src/features/photos/Photo.js
+++ src/features/photos/Photo.js
@@ -1,12 +1,17 @@
-
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
+
 
 export default function Photo({ photo, rover })
 	{
 	// console.log(`Photo.js ${photo.img_src}`)
 
-	const [fullScreen, setFullScreen] = useState(false);
+	const [searchParams,setSearchParams] = useSearchParams();
+	// Save the selected / clicked / fullScreen photo (and if loading URL with
+	// photo_id in searchParams, use that):
+	const [fullScreen, setFullScreen] = useState(c =>
+		searchParams.get("photo_id")
+		);
 
 	// If we're on a /photos page, indicate camera name;
 	// if we're on a /rovers/rover_name/camera_name, then the camera is already known:
@@ -15,6 +20,7 @@ export default function Photo({ photo, rover })
 		? `${photo.camera.full_name}`
 		: ""
 
+	// Set title & alt text for images:
 	const imageInfo = `Image Info:
 	Earth Date: ${photo.earth_date}
 	Sol: ${photo.sol}
@@ -22,21 +28,39 @@ export default function Photo({ photo, rover })
 	Rover: ${photo.rover.name}
 	photo id: ${photo.id}`;
 
+
 	const handleClick = (e) =>
 		{
-		console.log(`handleClick: e: ${e.currentTarget}  fullScreen: ${fullScreen.toString()}`)
+		console.log(`handleClick: e: ${e.currentTarget}`)
 // console.table(e.currentTarget)
-		setFullScreen( c => !c);
 		e.currentTarget.classList.toggle("fullScreen");
 		// Remove fullscreen styling from all photos:
 		const all = document.querySelectorAll(".fullScreen");
-		if (all !== null)
+		if (all.length === 0)
 			{
+			// Remove photo_id from search query in URL (no fullScreen photos):
+			searchParams.delete("photo_id");
+			// Remove ID of fullscreen / clicked photo:
+			setFullScreen( c => 0);
+			}
+		else
+			{
+			// Set photo_id in search query in URL:
+			searchParams.set("photo_id", e.currentTarget.id);
+			// Set ID of fullscreen / clicked photo:
+			// Note: e(vent) is null by time state updates, using
+			// intermediate variable instead:
+			const id = e.currentTarget.id;
+			setFullScreen( c => id);
+			// Remove fullScreen class from all images except the clicked upon one:
+			// (i.e. if clicking one image, then another, this removes the other's styling)
 			all.forEach(element => {
 				if (element !== e.currentTarget)
 					element.classList.remove("fullScreen");
 				});
 			}
+
+		setSearchParams(searchParams);
 		}
 
 
@@ -48,6 +72,9 @@ export default function Photo({ photo, rover })
 				src={photo.img_src}
 				title={imageInfo}
 				onClick={handleClick}
+				id={photo.id}
+				className={parseInt(fullScreen) === parseInt(photo.id)
+					? "fullScreen" : ""}
 				/>
 			<figcaption>
 				{photo.earth_date} #{photo.id}<br />{cameraDisplayName}
